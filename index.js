0e56dec089e0d4ec7b6a3d3d6867af0c1c23ff2b
Matthew Cook <matthewcook13987@gmail.com>
2021-02-11 22:13:43 +0000

    Initial commit for iss spotter project.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const {fetchMyIp} = require('./iss.js')
 
+fetchMyIp((error, ip) => {
+  if (error){
+    console.log("Didn't work!: ",error);
+    return;
+  }
+
+
+  console.log("It worked! IP: ", ip)
+})
--- iss.js
+++ iss.js
@@ -1,1 +1,10 @@
+const request = require('request')
 
+//https://api.ipify.org?format=json
+
+const fetchMyIp = function(callback){
+  request(url, error, callback)
+
+}
+
+module.exports = {fetchMyIp, };
