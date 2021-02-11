62c23d390e4950ad1d5e7a004d4bdb332df01fc9
Matthew Cook <matthewcook13987@gmail.com>
2021-02-11 22:33:42 +0000

    Implemented a function with error handling to grab the users IP address.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const {fetchMyIp} = require('./iss.js')
+const {fetchMyIp} = require('./iss.js');
 
 fetchMyIp((error, ip) => {
-  if (error){
+  if (error) {
     console.log("Didn't work!: ",error);
     return;
   }
 
 
-  console.log("It worked! IP: ", ip)
-})
+  console.log("It worked! IP: ", ip);
+});
--- iss.js
+++ iss.js
@@ -1,10 +1,23 @@
-const request = require('request')
+const request = require('request');
 
-//https://api.ipify.org?format=json
+let url = "https://api.ipify.org?format=json";
 
-const fetchMyIp = function(callback){
-  request(url, error, callback)
+const fetchMyIp = function(callback) {
+  request(url, (error, response, body) => {
+   
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
-}
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    console.log(body.body);
+  });
+};
 
 module.exports = {fetchMyIp, };
