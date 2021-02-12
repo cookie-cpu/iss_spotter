af0fce05e47ab0c89ec2ca5c67d7f96bf2902c59
Matthew Cook <matthewcook13987@gmail.com>
2021-02-12 00:33:25 +0000

    Corrected date formatting by converting utc string into a new date object

--- index.js
+++ index.js
@@ -9,51 +9,10 @@ nextISSTimesForMyLocation((error, passTimes) => {
   if (error){
     return console.log("Didn't work!:", error);
   }
-
-
   for (let pass of passTimes){
-    console.log(`Next pass is at ${pass.risetime} for ${pass.duration} seconds`)
-    //console.log(`Duration is ${pass.duration} \n Risetime is ${pass.risetime}`)
+    const date = new Date(0);
+    date.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass is at ${date} for ${duration} seconds`)
   }
-  //console.log(passTimes);
-  //console.log(`Next pass is at ${passTimes.risetime} for ${passTimes.duration} seconds`)
 })
-
-
-
-
-
-// fetchCoordsByIp("205.200.45.176", () => {
-//   //console.log(error, data)
-// })
-
-// fetchCoordsByIp('162.245.144.188', (error, coordinates) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned coordinates:' , coordinates);
-// });
-
-
-// fetchMyIp((error, ip) => {
-//   if (error) {
-//     console.log("Didn't work!: ",error);
-//     return;
-//   }
-//   console.log("It worked! IP: ", ip);
-// });
-
-// let sampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
-
-// fetchISSFlyOverTimes(sampleCoords, (error, data) => {
-//   if (error) {
-//     console.log("Didn't work! ", error);
-//     return
-//   }
-
-//   console.log("Success! Flyover times: ", data)
-// });
-
-
